3b918325342c876c2e3e3984209dea743ee71e8b
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-08-13 13:05:08 +0300

    Add models

---
+++ DataAccessLayer/Models/FlightClass.cs
@@ -0,0 +1,8 @@
+namespace DataAccessLayer.Models;
+
+public enum FlightClass
+{
+  Economy,
+  Business,
+  FirstClass
+}
---
+++ DataAccessLayer/Models/FlightClassDetails.cs
@@ -0,0 +1,10 @@
+namespace DataAccessLayer.Models;
+
+public class FlightClassDetails
+{
+  public FlightClass Class { get; set; }
+  
+  public decimal Price { get; set; }
+  
+  public int Capacity { get; set; }
+}
