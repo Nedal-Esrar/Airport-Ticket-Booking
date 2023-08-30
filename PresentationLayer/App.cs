87862a3c32f359a03cb838a405958beb486e133a
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-08-30 14:43:29 +0300

    Make use of DI containers
    DI containers is a better approach to manage dependencies.

--- PresentationLayer/App.cs
+++ PresentationLayer/App.cs
@@ -1,4 +1,6 @@
+using Microsoft.Extensions.DependencyInjection;
 using BusinessLogic.Services.Implementations;
+using BusinessLogic.Services.Interfaces;
 using DataAccessLayer.Csv.CsvImportService;
 using DataAccessLayer.Csv.Dtos;
 using DataAccessLayer.Csv.Mappers;
@@ -6,36 +8,31 @@ using DataAccessLayer.Csv.Parsers;
 using DataAccessLayer.Csv.Validators;
 using DataAccessLayer.Models;
 using DataAccessLayer.Repositories.Implementations;
+using DataAccessLayer.Repositories.Interfaces;
 using PresentationLayer;
 
-var validator = new Validator();
-var passengerParser = new PassengerCsvImportDtoParser();
-var passengerMapper = new PassengerCsvImportDtoToPassengerMapper();
-var passengerCsvImportService =
-  new ImportFromCsvService<PassengerCsvImportDto, Passenger>(passengerParser, validator, passengerMapper);
+var serviceProvider = new ServiceCollection()
+  .AddScoped<IValidator, Validator>()
+  .AddScoped<IParser<PassengerCsvImportDto>, PassengerCsvImportDtoParser>()
+  .AddScoped<IMapper<PassengerCsvImportDto, Passenger>, PassengerCsvImportDtoToPassengerMapper>()
+  .AddScoped<IImportFromCsvService<PassengerCsvImportDto, Passenger>,
+    ImportFromCsvService<PassengerCsvImportDto, Passenger>>()
+  .AddScoped<IParser<FlightCsvImportDto>, FlightCsvImportDtoParser>()
+  .AddScoped<IMapper<FlightCsvImportDto, Flight>, FlightCsvImportDtoToFlightMapper>()
+  .AddScoped<IImportFromCsvService<FlightCsvImportDto, Flight>, ImportFromCsvService<FlightCsvImportDto, Flight>>()
+  .AddScoped<IParser<BookingCsvImportDto>, BookingCsvImportDtoParser>()
+  .AddScoped<IMapper<BookingCsvImportDto, Booking>, BookingCsvImportDtoToBookingMapper>()
+  .AddScoped<IImportFromCsvService<BookingCsvImportDto, Booking>, ImportFromCsvService<BookingCsvImportDto, Booking>>()
+  .AddScoped<IPassengerRepository, CsvPassengerRepository>()
+  .AddScoped<IFlightRepository, CsvFlightRepository>()
+  .AddScoped<IBookingRepository, CsvBookingRepository>()
+  .AddScoped<IPassengerService, PassengerService>()
+  .AddScoped<IFlightService, FlightService>()
+  .AddScoped<IBookingService, BookingService>()
+  .AddScoped<MainMenu>();
 
-var passengerRepository = new CsvPassengerRepository(passengerCsvImportService);
-
-var flightParser = new FlightCsvImportDtoParser();
-var flightMapper = new FlightCsvImportDtoToFlightMapper();
-var flightCsvImportService =
-  new ImportFromCsvService<FlightCsvImportDto, Flight>(flightParser, validator, flightMapper);
-
-var flightRepository = new CsvFlightRepository(flightCsvImportService);
-
-var bookingParser = new BookingCsvImportDtoParser();
-var bookingMapper = new BookingCsvImportDtoToBookingMapper(flightRepository, passengerRepository);
-var bookingCsvImportService =
-  new ImportFromCsvService<BookingCsvImportDto, Booking>(bookingParser, validator, bookingMapper);
-
-var bookingRepository = new CsvBookingRepository(bookingCsvImportService);
-
-var passengerService = new PassengerService(passengerRepository);
-
-var flightService = new FlightService(flightRepository, bookingRepository, flightCsvImportService);
-
-var bookingService = new BookingService(bookingRepository, flightRepository, passengerRepository);
-
-var app = new MainMenu(bookingService, flightService, passengerService);
+var app = serviceProvider
+  .BuildServiceProvider()
+  .GetRequiredService<MainMenu>();
 
 app.Run();
