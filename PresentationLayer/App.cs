f43b6bb243d2325ab636d6171d46ea2171ed2820
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-03 15:43:01 +0300

    Modified app entry point to use newly introduced extensions

--- PresentationLayer/App.cs
+++ PresentationLayer/App.cs
@@ -1,38 +1,31 @@
+using BusinessLogic.Mappers;
+using BusinessLogic.PresentationLayerDtos;
+using BusinessLogic.Services.DateTimeProvider;
 using Microsoft.Extensions.DependencyInjection;
 using BusinessLogic.Services.Implementations;
 using BusinessLogic.Services.Interfaces;
-using DataAccessLayer.Csv.CsvImportService;
-using DataAccessLayer.Csv.Dtos;
-using DataAccessLayer.Csv.Mappers;
-using DataAccessLayer.Csv.Parsers;
-using DataAccessLayer.Csv.Validators;
-using DataAccessLayer.Models;
-using DataAccessLayer.Repositories.Implementations;
-using DataAccessLayer.Repositories.Interfaces;
+using DataAccess.Csv;
+using DataAccess.Csv.CsvImportService;
+using DataAccess.Csv.Dtos;
+using DataAccess.Csv.Mappers;
+using DataAccess.Csv.Parsers;
+using DataAccess.Csv.Validators;
+using DataAccess.Models;
+using DataAccess.Repositories.Implementations;
+using DataAccess.Repositories.Interfaces;
 using PresentationLayer;
 
 var serviceProvider = new ServiceCollection()
-  .AddScoped<IValidator, Validator>()
-  .AddScoped<IParser<PassengerCsvImportDto>, PassengerCsvImportDtoParser>()
-  .AddScoped<IMapper<PassengerCsvImportDto, Passenger>, PassengerCsvImportDtoToPassengerMapper>()
-  .AddScoped<IImportFromCsvService<PassengerCsvImportDto, Passenger>,
-    ImportFromCsvService<PassengerCsvImportDto, Passenger>>()
-  .AddScoped<IParser<FlightCsvImportDto>, FlightCsvImportDtoParser>()
-  .AddScoped<IMapper<FlightCsvImportDto, Flight>, FlightCsvImportDtoToFlightMapper>()
-  .AddScoped<IImportFromCsvService<FlightCsvImportDto, Flight>, ImportFromCsvService<FlightCsvImportDto, Flight>>()
-  .AddScoped<IParser<BookingCsvImportDto>, BookingCsvImportDtoParser>()
-  .AddScoped<IMapper<BookingCsvImportDto, Booking>, BookingCsvImportDtoToBookingMapper>()
-  .AddScoped<IImportFromCsvService<BookingCsvImportDto, Booking>, ImportFromCsvService<BookingCsvImportDto, Booking>>()
-  .AddScoped<IPassengerRepository, CsvPassengerRepository>()
-  .AddScoped<IFlightRepository, CsvFlightRepository>()
-  .AddScoped<IBookingRepository, CsvBookingRepository>()
-  .AddScoped<IPassengerService, PassengerService>()
-  .AddScoped<IFlightService, FlightService>()
-  .AddScoped<IBookingService, BookingService>()
+  .AddScopedServices()
+  .AddScopedMappers()
+  .AddScopedCsvServices()
+  .AddScopedCsvParsers()
+  .AddScopedCsvRepositories()
+  .AddScopedImportFromCsvServices()
   .AddScoped<MainMenu>();
 
 var app = serviceProvider
   .BuildServiceProvider()
   .GetRequiredService<MainMenu>();
 
-app.Run();
+await app.Run();
