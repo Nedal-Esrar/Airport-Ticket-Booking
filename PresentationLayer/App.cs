776d0f279757470d11b3ec3a36533b739c6b7c51
Nedal-Esrar <113736086+Nedal-Esrar@users.noreply.github.com>
2023-09-03 15:46:30 +0300

    Remove unneeded usings

--- PresentationLayer/App.cs
+++ PresentationLayer/App.cs
@@ -1,18 +1,4 @@
-using BusinessLogic.Mappers;
-using BusinessLogic.PresentationLayerDtos;
-using BusinessLogic.Services.DateTimeProvider;
 using Microsoft.Extensions.DependencyInjection;
-using BusinessLogic.Services.Implementations;
-using BusinessLogic.Services.Interfaces;
-using DataAccess.Csv;
-using DataAccess.Csv.CsvImportService;
-using DataAccess.Csv.Dtos;
-using DataAccess.Csv.Mappers;
-using DataAccess.Csv.Parsers;
-using DataAccess.Csv.Validators;
-using DataAccess.Models;
-using DataAccess.Repositories.Implementations;
-using DataAccess.Repositories.Interfaces;
 using PresentationLayer;
 
 var serviceProvider = new ServiceCollection()
